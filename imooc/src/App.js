de41ae79011ca9b61b969c12eb8992d4fb2239a2
silvacheng <cheng2681@gmail.com>
2018-10-30 20:38:55 +0800

    use antd-mobile && add plugin

--- imooc/src/App.js
+++ imooc/src/App.js
@@ -1,27 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css'
+import {Button, List} from 'antd-mobile'
+const Item = List.Item
+// import 'antd-mobile/dist/antd-mobile.css'
+class App extends React.Component {
 
-class App extends Component {
+  state = {
+    solders: ['11', '22', '33']
+  }
+  componentWillMount() {
+    // console.log('组件将要加载了。。')
+  }
+
+  componentDidMount() {
+    // console.log('组件加载完毕。。')
+  }
+  addSolder = () => {
+    console.log(`add solder!`)
+    this.setState({
+      solders: [...this.state.solders, '新兵' + Math.random()]
+    })
+  }
   render() {
+    // console.log('组件正在加载中。。。')
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Button type="primary" size="small" inline onClick={this.addSolder}>新增</Button>
+        <List
+          renderHeader={() => '士兵列表'}
+        >
+          {this.state.solders.map(v => {
+            return (
+              <Item key={v}>
+                {v}
+              </Item>
+            )
+          })}
+
+        </List>
+        {/* <ul>
+          {this.state.solders.map(v => {
+            return <li key={v}>{v}</li>
+          })}
+        </ul> */}
       </div>
-    );
+    )
   }
 }
 
